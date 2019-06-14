3dbee1f7aad0805ec3ae68ff73f4a21f537f333b
pinozu4 <pinozu4@gmail.com>
2019-06-14 14:46:06 -0500

    Actualizacion Proyecto

--- js/codigo.js
+++ js/codigo.js
@@ -8,4 +8,20 @@ $(document).ready(function(){
 		}, 1500, 'easeInOutExpo');
 	});
 
+	$('.nav a').on('click', function(){
+		$(this).addClass('active').siblings().removeClass('active')
+	});
+
+
+
+
+	$('.circulo').on({
+		mouseenter: function(){
+			$(this).find('.cuadro').fadeIn();
+		},
+		mouseleave: function(){
+			$(this).find('.cuadro').fadeOut();
+		}
+	})
+
 });
